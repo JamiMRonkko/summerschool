b5ddcd7225ea39d640071fd29febe724c404593a
training054 Student <training054@sisu-login1.csc.fi>
2019-06-29 12:08:05 +0300

    Did custom type a and b

--- mpi/datatypes/c/custom_type_a-b2.c
+++ mpi/datatypes/c/custom_type_a-b2.c
@@ -8,7 +8,8 @@ int main(int argc, char **argv)
     //TODO: Declare a variable storing the MPI datatype
     MPI_Datatype columntype;
     int i, j;
-
+    int displs[4];
+    int counts[4];
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
@@ -37,15 +38,21 @@ int main(int argc, char **argv)
         }
     }
 
+	for(i=0;i<4;i++){
+	counts[i] = i + 1;
+	displs[i] = i + 2 * i * 8; 
+	}
+
+
     //TODO: Create datatype that describes one column. Use MPI_Type_vector.
-	 MPI_Type_vector(8, 1, 8, MPI_INT, &columntype);
+	 MPI_Type_indexed(4, counts, displs, MPI_INT, &columntype);
 	 MPI_Type_commit(&columntype);
     //TODO: Send first column of matrix form rank 0 to rank 1
 	if(rank == 0){
-	MPI_Send(&array[0][1], 1, columntype, 1, 1, MPI_COMM_WORLD);
+	MPI_Send(array, 1, columntype, 1, 1, MPI_COMM_WORLD);
 	}
 	if(rank == 1){
-	MPI_Recv(&array[0][1], 1, columntype, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	MPI_Recv(array, 1, columntype, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 	}
     //TODO: free datatype
 
